9f86e45a8122d416854e429ac631a0345eba0a36
Kay <Kay.com>
2020-12-03 21:31:29 +0800

    12032131

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <template>
   <div id="app">
-    <div>
-      <el-row>
-        <el-button>默认按钮</el-button>
-        <el-button type="primary">主要按钮</el-button>
-        <el-button type="success">成功按钮</el-button>
-        <el-button type="info">信息按钮</el-button>
-        <el-button type="warning">警告按钮</el-button>
-        <el-button type="danger">危险按钮</el-button>
-      </el-row>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+export default {
+  name: 'app'
 }
+</script>
+
+<style>
 </style>
