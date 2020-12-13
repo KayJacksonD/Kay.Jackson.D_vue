7534b10c3f146c8418935a3b12a310a457b4c10f
Kay <Kay.com>
2020-12-13 11:41:34 +0800

    202012131141

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,36 +2,138 @@
   <el-container class="home-container">
     <!-- 头部区域 -->
     <el-header>
-      <div>
-        <img src="../assets/heima.png" alt="">
-        <span>电商后台管理系统</span>
-      </div>
-      <el-button type="info" @click="logout">退出</el-button>
+      <el-row>
+        <el-menu
+          mode="horizontal"
+          background-color="#000000"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-col :span="2">
+            <el-menu-item>
+              <el-tooltip
+                content="点击头像播放/暂停音乐"
+                placement="bottom"
+                effect="light"
+              >
+                <a @click="playVid()">
+                  <img
+                    id="logoID"
+                    src="../assets/headSculpture.jpg"
+                    alt=""
+                    width="50 px"
+                    height="50 px"
+                  />
+                  <video
+                    id="video"
+                    src="../assets/bgm.mp3"
+                    controls="controls"
+                    hidden="hidden"
+                    loop="loop"
+                  ></video>
+                </a>
+              </el-tooltip>
+            </el-menu-item>
+          </el-col>
+          <el-col :span="4">
+            <el-submenu index="1">
+              <template slot="title"> Why Kay . Jackson . D ?</template>
+              <el-menu-item index="1-1" @click="reloadCurrentPage()"
+                >Back to HomePage</el-menu-item
+              >
+              <el-menu-item index="1-2">What's Flesh</el-menu-item>
+              <el-menu-item index="1-3">About Jackson</el-menu-item>
+            </el-submenu>
+          </el-col>
+          <el-col :span="3">
+            <el-submenu index="2">
+              <template slot="title">My WorkSpace</template>
+              <a href="workFlow.html" target="right" style="color: black">
+                <el-menu-item index="2-1">myItems</el-menu-item>
+              </a>
+              <el-submenu index="2-2">
+                <template slot="title">option2</template>
+                <el-menu-item index="2-2-1">option1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-col>
+          <el-col :span="2">
+            <el-menu-item index="3">Message</el-menu-item>
+          </el-col>
+          <el-col :span="2">
+            <el-menu-item index="4"
+              ><a target="_blank">Account</a></el-menu-item
+            >
+          </el-col>
+          <el-col :span="5">
+            <el-menu-item>
+              <el-input
+                placeholder="Search"
+                v-model="input3"
+                class="input-with-select"
+              >
+                <i class="el-icon-search" slot="append"></i>
+              </el-input>
+            </el-menu-item>
+          </el-col>
+          <el-col :span="2">
+            <el-menu-item>
+              <a href="login.html">
+                <el-button type="text" size="medium">sin in</el-button>
+              </a>
+            </el-menu-item>
+          </el-col>
+          <el-col :span="2">
+            <el-menu-item>
+              <a href="index.html">
+                <i class="el-icon-switch-button"></i>
+              </a>
+            </el-menu-item>
+          </el-col>
+        </el-menu>
+      </el-row>
     </el-header>
-    <!-- 页面主体区域 -->
     <el-container>
       <!-- 侧边栏 -->
       <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
           <!-- 一级菜单 -->
-          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
               <i :class="iconsObj[item.id]"></i>
               <!-- 文本 -->
-              <span>{{item.authName}}</span>
+              <span>{{ item.authName }}</span>
             </template>
 
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
                 <!-- 文本 -->
-                <span>{{subItem.authName}}</span>
+                <span>{{ subItem.authName }}</span>
               </template>
             </el-menu-item>
           </el-submenu>
@@ -53,45 +155,56 @@ export default {
       // 左侧菜单数据
       menulist: [],
       iconsObj: {
-        '125': 'iconfont icon-user',
-        '103': 'iconfont icon-tijikongjian',
-        '101': 'iconfont icon-shangpin',
-        '102': 'iconfont icon-danju',
-        '145': 'iconfont icon-baobiao'
+        125: "iconfont icon-user",
+        103: "iconfont icon-tijikongjian",
+        101: "iconfont icon-shangpin",
+        102: "iconfont icon-danju",
+        145: "iconfont icon-baobiao",
       },
+      circleUrl: "/img/logo1.61b05148.png", // 该属性暂没调试通过
+      ifAutoplay: true,
       // 是否折叠
       isCollapse: false,
       // 被激活的链接地址
-      activePath: ''
-    }
+      activePath: "",
+    };
   },
   created() {
-    this.getMenuList()
-    this.activePath = window.sessionStorage.getItem('activePath')
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem("activePath");
   },
   methods: {
+    playVid() {
+      let vo = document.getElementById("video");
+      if (this.ifAutoplay == true) {
+        vo.play();
+      } else {
+        vo.pause();
+      }
+      this.ifAutoplay = !this.ifAutoplay;
+    },
     logout() {
-      window.sessionStorage.clear()
-      this.$router.push('/login')
+      window.sessionStorage.clear();
+      this.$router.push("/login");
     },
     // 获取所有的菜单
     async getMenuList() {
-      const { data: res } = await this.$http.get('menus')
-      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
-      this.menulist = res.data
-      console.log(res)
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menulist = res.data;
+      console.log(res);
     },
     // 点击按钮，切换菜单的折叠与展开
     toggleCollapse() {
-      this.isCollapse = !this.isCollapse
+      this.isCollapse = !this.isCollapse;
     },
     // 保存链接的激活状态
     saveNavState(activePath) {
-      window.sessionStorage.setItem('activePath', activePath)
-      this.activePath = activePath
-    }
-  }
-}
+      window.sessionStorage.setItem("activePath", activePath);
+      this.activePath = activePath;
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -99,20 +212,20 @@ export default {
   height: 100%;
 }
 .el-header {
-  background-color: #373d41;
-  display: flex;
-  justify-content: space-between;
-  padding-left: 0;
-  align-items: center;
-  color: #fff;
-  font-size: 20px;
-  > div {
-    display: flex;
-    align-items: center;
-    span {
-      margin-left: 15px;
-    }
-  }
+  background-color: #000000;
+  // display: flex;
+  // justify-content: space-between;
+  // padding-left: 0;
+  // align-items: center;
+  // color: #fff;
+  // font-size: 20px;
+  // > div {
+  //   display: flex;
+  //   align-items: center;
+  //   span {
+  //     margin-left: 15px;
+  //   }
+  // }
 }
 
 .el-aside {
@@ -138,5 +251,11 @@ export default {
   text-align: center;
   letter-spacing: 0.2em;
   cursor: pointer;
+}
+
+#logoID {
+  border-radius: 25px;
+  -webkit-border-radius: 25px;
+  -moz-border-radius: 25px;
 }
 </style>
